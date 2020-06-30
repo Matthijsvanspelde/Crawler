200cf009217218c02dcf8fb54fe135a298077d5c
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-06-30 17:07:10 +0200

    Added RoomTiles

---
+++ Assets/Scripts/Map/TileFactory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class TileFactory : MonoBehaviour
+{
+    public int openingDir;
+
+    private void Update()
+    {
+        if (openingDir == 1)
+        {
+
+        }
+        else if (openingDir == 2)
+        {
+
+        }
+        else if (openingDir == 3)
+        {
+
+        }
+        else if (openingDir == 4)
+        {
+
+        }
+    }
+}
