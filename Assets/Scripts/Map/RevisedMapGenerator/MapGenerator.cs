0f761eb1a5c64fb127c7d83f7badb9f6abae439c
Luc Somers <lucsomers@hotmail.com>
2020-07-05 20:39:19 +0200

    Revised MapGenerator clean up of code

--- Assets/Scripts/Map/RevisedMapGenerator/MapGenerator.cs
+++ Assets/Scripts/Map/RevisedMapGenerator/MapGenerator.cs
@@ -3,39 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SideCalculator))]
 public class MapGenerator : MonoBehaviour
 {
     [Header("Tiles")]
-    [SerializeField] private List<Tile> tilePrefabs = new List<Tile>();
+    [SerializeField] private List<Tile> tilePrefabsList = new List<Tile>();
     [SerializeField] private Tile startTile;
-    [SerializeField] private Tile emptyTile;
     [Header("Size")]
     [SerializeField] private int xSize = 4;
     [SerializeField] private int ySize = 4;
 
-    private Tile[,] tileGrid;
+    private Tile[,] tileMapGrid;
+    private List<Tile> runThroughMap = new List<Tile>();
+    private List<Tile> tileChanceList = new List<Tile>();
+
+    private SideCalculator sideCalc;
 
     private int LoopedAmount = 1;
     private int TotalChance = 0;
 
-    private List<Tile> map = new List<Tile>();
-
-    private bool canBeEndPiece = false;
     private bool KeepFilling = true;
 
-    private List<Tile> tilePool = new List<Tile>();
+    private void Awake()
+    {
+        sideCalc = GetComponent<SideCalculator>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        tileGrid = new Tile[xSize, ySize];
+        tileMapGrid = new Tile[xSize, ySize];
         SpawnTile(startTile, new Vector3(0,0,0), (Side)UnityEngine.Random.Range(0,4),xSize / 2, ySize / 2);
         FillMap();
     }
 
     private void FillMap()
     {
-        Tile t = map[0];
+        Tile t = runThroughMap[0];
 
         while (KeepFilling)
         {
@@ -43,166 +47,83 @@ public class MapGenerator : MonoBehaviour
             //yield return new WaitForSeconds(2);
             // yield return null;
 
-            t = map[LoopedAmount];
+            t = runThroughMap[LoopedAmount];
             LoopedAmount++;
-            if (LoopedAmount >= map.Count)
+            if (LoopedAmount >= runThroughMap.Count)
             {
                 KeepFilling = false;
             }
         }
-        //StartCoroutine(testFill());
     }
-
-    //IEnumerator testFill()
-    //{
-        
-    //}
     
     private void CreateTilesAroundTile(Tile t)
     {
         for (int SideNumber = 0; SideNumber < 4; SideNumber++)
         {
-            float SideDistance = GetSideDistance(t, (Side)SideNumber);
+            FillSide(t, (Side)SideNumber);
+        }
+    }
 
-            //if true then we have space to spawn a new tile
-            if (SideDistance > 0 && !float.IsNaN(SideDistance))
+    private void FillSide(Tile t, Side sideToSpawnAt)
+    {
+        float SideDistance = sideCalc.GetSideDistance(t, sideToSpawnAt);
+
+        //if true then we have space to spawn a new tile
+        if (SideDistance > 0 && !float.IsNaN(SideDistance))
+        {
+            Tile ToSpawn = null;
+            float ToSpawnSideDistance = GetSpawnDistance(sideToSpawnAt, ref ToSpawn);
+
+            Vector3 spawnLocation = sideCalc.CalculateSpawnLocation(t, SideDistance, ToSpawnSideDistance, (Side)sideToSpawnAt);
+
+            int xpos = sideCalc.GetxPosFromSide(t, (Side)sideToSpawnAt);
+            int ypos = sideCalc.GetyPosFromSide(t, (Side)sideToSpawnAt);
+
+            if (xpos >= 0 && xpos < xSize && ypos >= 0 && ypos < ySize)
             {
-                Tile ToSpawn = null;
-
-                float ToSpawnSideDistance = 0;
-
-                while (ToSpawnSideDistance == 0)
+                if (tileMapGrid[xpos, ypos] == null)
                 {
-                    ToSpawn = GetRandomTile();
-                    ToSpawnSideDistance = GetSideDistance(ToSpawn, GetOpositeSide((Side)SideNumber));
-                }
-
-                t.FillAttachmentPoint((Side)SideNumber);
-
-                Vector3 spawnLocation = CalculateSpawnLocation(t,SideDistance,ToSpawnSideDistance,(Side)SideNumber);
-
-                int xpos = GetxPosFromSide(t, (Side)SideNumber);
-                int ypos = GetyPosFromSide(t, (Side)SideNumber);
-
-                if (xpos >= 0 && xpos < xSize && ypos >= 0 && ypos < ySize)
-                {
-                    if (tileGrid[xpos, ypos] == null)
-                    {
-                        SpawnTile(ToSpawn, spawnLocation, GetOpositeSide((Side)SideNumber), xpos, ypos);
-                    }
+                    SpawnTile(ToSpawn, spawnLocation, sideCalc.GetOpositeSide((Side)sideToSpawnAt), xpos, ypos);
                 }
             }
         }
     }
 
-    private int GetxPosFromSide(Tile origin, Side s)
+    private float GetSpawnDistance(Side side, ref Tile ToSpawn)
     {
-        switch (s)
+        float ToSpawnSideDistance = 0;
+
+        while (ToSpawnSideDistance == 0)
         {
-            case Side.TOP:
-                return origin.xPos;
-            case Side.BOTTOM:
-                return origin.xPos;
-            case Side.RIGHT:
-                return origin.xPos + 1;
-            case Side.LEFT:
-                return origin.xPos - 1;
-            default:
-                return 0;
-        }
-    }
-
-    private int GetyPosFromSide(Tile origin, Side s)
-    {
-        switch (s)
-        {
-            case Side.TOP:
-                return origin.yPos + 1;
-            case Side.BOTTOM:
-                return origin.yPos - 1;
-            case Side.RIGHT:
-                return origin.yPos;
-            case Side.LEFT:
-                return origin.yPos;
-            default:
-                return 0;
-        }
-    }
-
-    private Vector3 CalculateSpawnLocation(Tile t, float sideDistance, float toSpawnSideDistance, Side side)
-    {
-        Vector3 PosToReturn = Vector3.zero;
-
-        Vector3 TilePos = t.transform.position;
-
-        switch (side)
-        {
-            case Side.TOP:
-                PosToReturn = new Vector3(TilePos.x, TilePos.y, TilePos.z + sideDistance + toSpawnSideDistance);
-                break;
-            case Side.BOTTOM:
-                PosToReturn = new Vector3(TilePos.x, TilePos.y, TilePos.z - sideDistance - toSpawnSideDistance);
-                break;
-            case Side.RIGHT:
-                PosToReturn = new Vector3(TilePos.x + sideDistance + toSpawnSideDistance, TilePos.y, TilePos.z);
-                break;
-            case Side.LEFT:
-                PosToReturn = new Vector3(TilePos.x - sideDistance - toSpawnSideDistance, TilePos.y, TilePos.z);
-                break;
-            default:
-                break;
+            ToSpawn = GetRandomTile();
+            ToSpawnSideDistance = sideCalc.GetSideDistance(ToSpawn, sideCalc.GetOpositeSide((Side)side));
         }
 
-        return PosToReturn;
-    }
-
-    private Side GetOpositeSide(Side s)
-    {
-        switch (s)
-        {
-            case Side.TOP:
-                return Side.BOTTOM;
-            case Side.BOTTOM:
-                return Side.TOP;
-            case Side.RIGHT:
-                return Side.LEFT;
-            case Side.LEFT:
-                return Side.RIGHT;
-            default:
-                return Side.BOTTOM;
-        }
-    }
-
-    private float GetSideDistance(Tile t, Side s)
-    {
-        switch (s)
-        {
-            case Side.TOP:
-                return t.GetDistanceFromSide(Side.TOP);
-            case Side.BOTTOM:
-                return t.GetDistanceFromSide(Side.BOTTOM);
-            case Side.RIGHT:
-                return t.GetDistanceFromSide(Side.RIGHT);
-            case Side.LEFT:
-                return t.GetDistanceFromSide(Side.LEFT);
-            default:
-                return 0;
-        }
+        return ToSpawnSideDistance;
     }
 
     private void SpawnTile(Tile TileToSpawn, Vector3 SpawnLocation, Side s,int xpos,int ypos)
     {
         GameObject ToSpawn = Instantiate(TileToSpawn.PrefabObject);
-        ToSpawn.SetActive(true);
-        ToSpawn.transform.position = SpawnLocation;
+        SetGameObject(SpawnLocation, ToSpawn);
 
         Tile tile = ToSpawn.GetComponent<Tile>();
+        SetTile(xpos, ypos, tile);
 
+        tileMapGrid[xpos, ypos] = tile;
+        runThroughMap.Add(tile);
+    }
+
+    private void SetTile(int xpos, int ypos, Tile tile)
+    {
         tile.xPos = xpos;
         tile.yPos = ypos;
+    }
 
-        tileGrid[xpos, ypos] = tile;
-        map.Add(tile);
+    private void SetGameObject(Vector3 SpawnLocation, GameObject ToSpawn)
+    {
+        ToSpawn.SetActive(true);
+        ToSpawn.transform.position = SpawnLocation;
     }
 
     private Tile GetRandomTile()
@@ -212,18 +133,18 @@ public class MapGenerator : MonoBehaviour
 
         int randomIndex = UnityEngine.Random.Range(0, TotalChance);
 
-        Tile tileToReturn = tilePool[randomIndex];
+        Tile tileToReturn = tileChanceList[randomIndex];
         return tileToReturn;
     }
 
     private void SetTotalChance()
     {
-        foreach (Tile tile in tilePrefabs)
+        foreach (Tile tile in tilePrefabsList)
         {
             for (int i = 0; i < tile.Data.ChanceToSpawn; i++)
             {
                 TotalChance++;
-                tilePool.Add(tile);
+                tileChanceList.Add(tile);
             }
         }
     }
---
+++ Assets/Scripts/Map/RevisedMapGenerator/SideCalculator.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideCalculator : MonoBehaviour
+{
+    public int GetxPosFromSide(Tile origin, Side s)
+    {
+        switch (s)
+        {
+            case Side.TOP:
+                return origin.xPos;
+            case Side.BOTTOM:
+                return origin.xPos;
+            case Side.RIGHT:
+                return origin.xPos + 1;
+            case Side.LEFT:
+                return origin.xPos - 1;
+            default:
+                return 0;
+        }
+    }
+
+    public int GetyPosFromSide(Tile origin, Side s)
+    {
+        switch (s)
+        {
+            case Side.TOP:
+                return origin.yPos + 1;
+            case Side.BOTTOM:
+                return origin.yPos - 1;
+            case Side.RIGHT:
+                return origin.yPos;
+            case Side.LEFT:
+                return origin.yPos;
+            default:
+                return 0;
+        }
+    }
+
+    public Vector3 CalculateSpawnLocation(Tile t, float sideDistance, float toSpawnSideDistance, Side side)
+    {
+        Vector3 PosToReturn = Vector3.zero;
+
+        Vector3 TilePos = t.transform.position;
+
+        switch (side)
+        {
+            case Side.TOP:
+                PosToReturn = new Vector3(TilePos.x, TilePos.y, TilePos.z + sideDistance + toSpawnSideDistance);
+                break;
+            case Side.BOTTOM:
+                PosToReturn = new Vector3(TilePos.x, TilePos.y, TilePos.z - sideDistance - toSpawnSideDistance);
+                break;
+            case Side.RIGHT:
+                PosToReturn = new Vector3(TilePos.x + sideDistance + toSpawnSideDistance, TilePos.y, TilePos.z);
+                break;
+            case Side.LEFT:
+                PosToReturn = new Vector3(TilePos.x - sideDistance - toSpawnSideDistance, TilePos.y, TilePos.z);
+                break;
+            default:
+                break;
+        }
+
+        return PosToReturn;
+    }
+
+    public Side GetOpositeSide(Side s)
+    {
+        switch (s)
+        {
+            case Side.TOP:
+                return Side.BOTTOM;
+            case Side.BOTTOM:
+                return Side.TOP;
+            case Side.RIGHT:
+                return Side.LEFT;
+            case Side.LEFT:
+                return Side.RIGHT;
+            default:
+                return Side.BOTTOM;
+        }
+    }
+
+    public float GetSideDistance(Tile t, Side s)
+    {
+        switch (s)
+        {
+            case Side.TOP:
+                return t.GetDistanceFromSide(Side.TOP);
+            case Side.BOTTOM:
+                return t.GetDistanceFromSide(Side.BOTTOM);
+            case Side.RIGHT:
+                return t.GetDistanceFromSide(Side.RIGHT);
+            case Side.LEFT:
+                return t.GetDistanceFromSide(Side.LEFT);
+            default:
+                return 0;
+        }
+    }
+}
