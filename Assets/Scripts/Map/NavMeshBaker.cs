75632d6ec92fa69ffb5837426e061c8904197a4e
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-07-06 12:23:36 +0200

    Added NavMeshBakes.cs

---
+++ Assets/Scripts/Map/NavMeshBaker.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshBaker : MonoBehaviour
+{
+    NavMeshSurface  navMeshSurface;
+    
+    void Start()
+    {
+        navMeshSurface = GetComponent<NavMeshSurface>();
+        navMeshSurface.BuildNavMesh();
+    }
+
+}
