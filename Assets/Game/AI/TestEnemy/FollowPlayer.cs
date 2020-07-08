95bd59b5bd35109c3440dc3164e9efe45597300c
Luc Somers <lucsomers@hotmail.com>
2020-07-08 16:31:54 +0200

    Fixed bug where enemy keeps attacking

--- Assets/Game/AI/TestEnemy/FollowPlayer.cs
+++ Assets/Game/AI/TestEnemy/FollowPlayer.cs
@@ -37,6 +37,7 @@ public class FollowPlayer : AIState
             {
                 InAttackRange.Invoke();
                 Done = true;
+                StopAllCoroutines();
             }
         }
     }
