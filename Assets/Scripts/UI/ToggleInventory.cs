aa2dc7a3fc329be307fe8cefa8485f3196b51710
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-07-14 19:07:41 +0200

    inventory

---
+++ Assets/Scripts/UI/ToggleInventory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ToggleInventory : MonoBehaviour
+{
+    public GameObject menu;
+    private bool isShowing = false;
+
+    private void Start()
+    {
+        menu.SetActive(isShowing);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isShowing = !isShowing;
+            menu.SetActive(isShowing);
+        }
+    }
+}
